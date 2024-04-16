6a8c14fddc35f7806e0bc8f4f90f10d1e01f97c8
kiyoung98 <kyseong@postech.ac.kr>
2024-04-16 07:52:07 +0000

    initial commit

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,8 +1,8 @@
-CUDA_VISIBLE_DEVICES=0 python src/train.py --wandb --z_scale 10 --terminal_std 0.001 --learning_rate 0.1 --force &
-CUDA_VISIBLE_DEVICES=1 python src/train.py --wandb --z_scale 10 --terminal_std 0.001 --learning_rate 0.01 --force &
-CUDA_VISIBLE_DEVICES=2 python src/train.py --wandb --z_scale 10 --terminal_std 0.001 --learning_rate 0.001 --force &
-CUDA_VISIBLE_DEVICES=3 python src/train.py --wandb --z_scale 10 --terminal_std 0.001 --learning_rate 0.0001 --force &
-CUDA_VISIBLE_DEVICES=4 python src/train.py --wandb --z_scale 10 --terminal_std 0.01 --learning_rate 0.1 --force &
-CUDA_VISIBLE_DEVICES=5 python src/train.py --wandb --z_scale 10 --terminal_std 0.01 --learning_rate 0.01 --force &
-CUDA_VISIBLE_DEVICES=6 python src/train.py --wandb --z_scale 10 --terminal_std 0.01 --learning_rate 0.001 --force &
-CUDA_VISIBLE_DEVICES=7 python src/train.py --wandb --z_scale 10 --terminal_std 0.01 --learning_rate 0.0001 --force
+CUDA_VISIBLE_DEVICES=0 python src/train.py --project alanine --wandb --force --terminal_std 1 --learning_rate 0.1 &
+CUDA_VISIBLE_DEVICES=1 python src/train.py --project alanine --wandb --force --terminal_std 0.1 --learning_rate 0.1 &
+CUDA_VISIBLE_DEVICES=2 python src/train.py --project alanine --wandb --force --terminal_std 0.01 --learning_rate 0.1 &
+CUDA_VISIBLE_DEVICES=3 python src/train.py --project alanine --wandb --force --terminal_std 10 --learning_rate 0.1 &
+CUDA_VISIBLE_DEVICES=4 python src/train.py --project alanine --wandb --force --terminal_std 1 --learning_rate 0.01 &
+CUDA_VISIBLE_DEVICES=5 python src/train.py --project alanine --wandb --force --terminal_std 0.1 --learning_rate 0.01 &
+CUDA_VISIBLE_DEVICES=6 python src/train.py --project alanine --wandb --force --terminal_std 0.01 --learning_rate 0.01 &
+CUDA_VISIBLE_DEVICES=7 python src/train.py --project alanine --wandb --force --terminal_std 10 --learning_rate 0.01
