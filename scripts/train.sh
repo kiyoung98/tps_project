a33be581c39087413345f9e66894da0ffa57618a
kiyoung98 <kyseong@postech.ac.kr>
2024-04-16 07:04:00 +0000

    initial commit

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,8 @@
-python src/train.py
+CUDA_VISIBLE_DEVICES=0 python src/train.py --wandb --z_scale 10 --terminal_std 0.001 --learning_rate 0.1 --force &
+CUDA_VISIBLE_DEVICES=1 python src/train.py --wandb --z_scale 10 --terminal_std 0.001 --learning_rate 0.01 --force &
+CUDA_VISIBLE_DEVICES=2 python src/train.py --wandb --z_scale 10 --terminal_std 0.001 --learning_rate 0.001 --force &
+CUDA_VISIBLE_DEVICES=3 python src/train.py --wandb --z_scale 10 --terminal_std 0.001 --learning_rate 0.0001 --force &
+CUDA_VISIBLE_DEVICES=4 python src/train.py --wandb --z_scale 10 --terminal_std 0.01 --learning_rate 0.1 --force &
+CUDA_VISIBLE_DEVICES=5 python src/train.py --wandb --z_scale 10 --terminal_std 0.01 --learning_rate 0.01 --force &
+CUDA_VISIBLE_DEVICES=6 python src/train.py --wandb --z_scale 10 --terminal_std 0.01 --learning_rate 0.001 --force &
+CUDA_VISIBLE_DEVICES=7 python src/train.py --wandb --z_scale 10 --terminal_std 0.01 --learning_rate 0.0001 --force
