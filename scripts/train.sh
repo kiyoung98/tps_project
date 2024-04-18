37026f43fc7c841826c43dac95ae1f91f6de4ab5
kiyoung98 <kyseong@postech.ac.kr>
2024-04-18 03:24:14 +0000

    change setting

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,1 +1,5 @@
-python src/train.py
+CUDA_VISIBLE_DEVICES=0 python src/train.py --project alanine
+
+CUDA_VISIBLE_DEVICES=0 python src/train.py --molecule chignolin --project chignolin --start_std 0.15 --trains_per_sample 200 --terminal_std 0.5
+
+CUDA_VISIBLE_DEVICES=0 python src/train.py --molecule poly --project poly --start_std 0.15 --trains_per_sample 1000 --terminal_std 0.1
