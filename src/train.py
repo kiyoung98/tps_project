9195dfe2b9c410f6c1e9a84882b6d339a4952e65
kiyoung98 <kyseong@postech.ac.kr>
2024-05-02 07:51:28 +0000

    change default max_grad_norm to 10

--- src/train.py
+++ src/train.py
@@ -44,7 +44,7 @@ parser.add_argument('--learning_rate', default=1e-3, type=float)
 parser.add_argument('--num_rollouts', default=10000, type=int, help='Number of rollouts (or sampling)')
 parser.add_argument('--trains_per_rollout', default=2000, type=int, help='Number of training per rollout in a rollout')
 parser.add_argument('--buffer_size', default=2048, type=int, help='Size of buffer which stores sampled paths')
-parser.add_argument('--max_grad_norm', default=1, type=int, help='Maximum norm of gradient to clip')
+parser.add_argument('--max_grad_norm', default=10, type=int, help='Maximum norm of gradient to clip')
 
 args = parser.parse_args()
 
