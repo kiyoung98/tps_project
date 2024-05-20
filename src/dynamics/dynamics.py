0eb5c7fb636ec5485b7d54524587c09be9a617ef
hyun <shpark26@postech.ac.kr>
2024-05-20 09:24:19 +0000

    upd - timestep

--- src/dynamics/dynamics.py
+++ src/dynamics/dynamics.py
@@ -109,7 +109,7 @@ class Poly(BaseDynamics):
         integrator = mm.LangevinIntegrator(
             self.temperature,  
             self.friction_coefficient,  
-            self.timestep * 2,
+            self.timestep,
         ) 
 
         integrator.setConstraintTolerance(0.00001)
