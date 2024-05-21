d690395a815428876e20a5770ce5e81408c6d32e
kiyoung98 <kyseong@postech.ac.kr>
2024-05-21 03:28:13 +0000

    initial commit

--- scripts/reproduce_table.sh
+++ scripts/reproduce_table.sh
@@ -1,3 +1,3 @@
 CUDA_VISIBLE_DEVICES=0 python src/eval.py --model_path model/alanine/pot_fix.pt --bias_scale 20000
 
-CUDA_VISIBLE_DEVICES=1 python src/eval.py --model_path model/alanine/force_fix.pt --force --bias_scale 1000
+# CUDA_VISIBLE_DEVICES=1 python src/eval.py --model_path model/alanine/force_fix.pt --force --bias_scale 1000
--- scripts/reproduce_train.sh
+++ scripts/reproduce_train.sh
@@ -6,8 +6,3 @@ CUDA_VISIBLE_DEVICES=4 python src/baseline_setup_train.py --seed 0 --save_dir re
 CUDA_VISIBLE_DEVICES=5 python src/baseline_setup_train.py --seed 1 --save_dir results/alanine/force/1/ --force &
 CUDA_VISIBLE_DEVICES=6 python src/baseline_setup_train.py --seed 2 --save_dir results/alanine/force/2/ --force &
 CUDA_VISIBLE_DEVICES=7 python src/baseline_setup_train.py --seed 3 --save_dir results/alanine/force/3/ --force 
-
-
-
-
-# CUDA_VISIBLE_DEVICES=1 python src/baseline_setup_train.py --force
