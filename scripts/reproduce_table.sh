2805be8fa317dbfd465a6ca699d8fc2c3efce857
kiyoung98 <kyseong@postech.ac.kr>
2024-05-22 17:44:17 +0000

    .

--- scripts/reproduce_table.sh
+++ scripts/reproduce_table.sh
@@ -2,5 +2,6 @@ CUDA_VISIBLE_DEVICES=0 python src/eval.py --model_path model/alanine/pot_fix.pt 
 
 # CUDA_VISIBLE_DEVICES=1 python src/eval.py --model_path model/alanine/force_fix.pt --force --bias_scale 1000
 
+# CUDA_VISIBLE_DEVICES=2 python src/eval.py --model_path model/histidine/pot_flex.pt --num_steps 1000 --sigma 0.1 --molecule histidine
 
-# CUDA_VISIBLE_DEVICES=0 python src/eval.py --model_path model/histidine/histidine_table.pt --sigma 0.1 --num_steps 1000 --molecule histidine --num_samples 4
+# CUDA_VISIBLE_DEVICES=3 python src/eval.py --model_path model/histidine/force_flex.pt --force --bias_scale 0.0001 --num_steps 1000 --sigma 0.1 --molecule histidine
