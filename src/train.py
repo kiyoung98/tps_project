dad4289943563a167bc6969cdd06da8b59566594
kiyoung98 <kyseong@postech.ac.kr>
2024-07-15 15:56:29 +0000

    initial commit

--- src/train.py
+++ src/train.py
@@ -38,7 +38,7 @@ parser.add_argument("--feat_aug", default="dist", type=str)
 parser.add_argument(
     "--bias_scale", default=0.01, type=float, help="Scale factor of bias"
 )
-parser.add_argument("--target_reward_scale", default=10, type=float)
+parser.add_argument("--target_reward_scale", default=20, type=float)
 parser.add_argument("--timestep", default=1, type=float, help="Timestep of integrator")
 parser.add_argument(
     "--sigma", default=0.05, type=float, help="Control reward of arrival"
