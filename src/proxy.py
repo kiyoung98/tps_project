82085749fef459ed502f48fcdd5f701f95454c2e
kiyoung98 <kyseong@postech.ac.kr>
2024-07-21 00:13:23 +0000

    initial commit

--- src/proxy.py
+++ src/proxy.py
@@ -7,17 +7,14 @@ class Alanine(nn.Module):
         super().__init__()
 
         self.force = args.force
-        self.feat_aug = args.feat_aug
+        self.dist_feat = args.dist_feat
 
         self.num_particles = md.num_particles
-        if args.feat_aug == "dist":
+        self.output_dim = md.num_particles * 3 if self.force else 1
+        if args.dist_feat:
             self.input_dim = md.num_particles * (3 + 1)
-        elif args.feat_aug in ["rel_pos", "norm_rel_pos"]:
-            self.input_dim = md.num_particles * (3 + 3)
         else:
             self.input_dim = md.num_particles * 3
-
-        self.output_dim = md.num_particles * 3 if self.force else 1
 
         self.mlp = nn.Sequential(
             nn.Linear(self.input_dim, 128),
@@ -39,16 +36,9 @@ class Alanine(nn.Module):
     def forward(self, pos, target):
         if not self.force:
             pos.requires_grad = True
-        if self.feat_aug == "dist":
+        if self.dist_feat:
             dist = torch.norm(pos - target, dim=-1, keepdim=True)
             pos_ = torch.cat([pos, dist], dim=-1)
-        elif self.feat_aug == "rel_pos":
-            pos_ = torch.cat([pos, pos - target], dim=-1)
-        elif self.feat_aug == "norm_rel_pos":
-            pos_ = torch.cat(
-                [pos, (pos - target) / torch.norm(pos - target, dim=-1, keepdim=True)],
-                dim=-1,
-            )
         else:
             pos_ = pos
 
@@ -69,16 +59,14 @@ class Chignolin(nn.Module):
         super().__init__()
 
         self.force = args.force
-        self.feat_aug = args.feat_aug
+        self.dist_feat = args.dist_feat
 
         self.num_particles = md.num_particles
-        if args.feat_aug == "dist":
+        self.output_dim = md.num_particles * 3 if self.force else 1
+        if args.dist_feat:
             self.input_dim = md.num_particles * (3 + 1)
-        elif args.feat_aug in ["rel_pos", "norm_rel_pos"]:
-            self.input_dim = md.num_particles * (3 + 3)
         else:
             self.input_dim = md.num_particles * 3
-        self.output_dim = md.num_particles * 3 if self.force else 1
 
         self.mlp = nn.Sequential(
             nn.Linear(self.input_dim, 512),
@@ -101,16 +89,9 @@ class Chignolin(nn.Module):
     def forward(self, pos, target):
         if not self.force:
             pos.requires_grad = True
-        if self.feat_aug == "dist":
+        if self.dist_feat:
             dist = torch.norm(pos - target, dim=-1, keepdim=True)
             pos_ = torch.cat([pos, dist], dim=-1)
-        elif self.feat_aug == "rel_pos":
-            pos_ = torch.cat([pos, pos - target], dim=-1)
-        elif self.feat_aug == "norm_rel_pos":
-            pos_ = torch.cat(
-                [pos, (pos - target) / torch.norm(pos - target, dim=-1, keepdim=True)],
-                dim=-1,
-            )
         else:
             pos_ = pos
 
@@ -133,8 +114,11 @@ class Poly(nn.Module):
         self.force = args.force
 
         self.num_particles = md.num_particles
-        self.input_dim = md.num_particles * (3 + 1)
         self.output_dim = md.num_particles * 3 if self.force else 1
+        if args.dist_feat:
+            self.input_dim = md.num_particles * (3 + 1)
+        else:
+            self.input_dim = md.num_particles * 3
 
         self.mlp = nn.Sequential(
             nn.Linear(self.input_dim, 256),
@@ -160,68 +144,6 @@ class Poly(nn.Module):
 
         dist = torch.norm(pos - target, dim=-1, keepdim=True)
         pos_ = torch.cat([pos, dist], dim=-1)
-
-        out = self.mlp(pos_.reshape(-1, self.input_dim))
-
-        if not self.force:
-            force = -torch.autograd.grad(
-                out.sum(), pos, create_graph=True, retain_graph=True
-            )[0]
-        else:
-            force = out.view(*pos.shape)
-
-        return force
-
-
-class Histidine(nn.Module):
-    def __init__(self, args, md):
-        super().__init__()
-
-        self.force = args.force
-        self.feat_aug = args.feat_aug
-
-        self.num_particles = md.num_particles
-        if args.feat_aug == "dist":
-            self.input_dim = md.num_particles * (3 + 1)
-        elif args.feat_aug in ["rel_pos", "norm_rel_pos"]:
-            self.input_dim = md.num_particles * (3 + 3)
-        else:
-            self.input_dim = md.num_particles * 3
-        self.output_dim = md.num_particles * 3 if self.force else 1
-
-        self.mlp = nn.Sequential(
-            nn.Linear(self.input_dim, 256),
-            nn.ReLU(),
-            nn.Linear(256, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 512),
-            nn.ReLU(),
-            nn.Linear(512, 256),
-            nn.ReLU(),
-            nn.Linear(256, self.output_dim, bias=False),
-        )
-
-        self.log_z = nn.Parameter(torch.tensor(0.0))
-
-        self.to(args.device)
-
-    def forward(self, pos, target):
-        if not self.force:
-            pos.requires_grad = True
-        if self.feat_aug == "dist":
-            dist = torch.norm(pos - target, dim=-1, keepdim=True)
-            pos_ = torch.cat([pos, dist], dim=-1)
-        elif self.feat_aug == "rel_pos":
-            pos_ = torch.cat([pos, pos - target], dim=-1)
-        elif self.feat_aug == "norm_rel_pos":
-            pos_ = torch.cat(
-                [pos, (pos - target) / torch.norm(pos - target, dim=-1, keepdim=True)],
-                dim=-1,
-            )
-        else:
-            pos_ = pos
 
         out = self.mlp(pos_.reshape(-1, self.input_dim))
 
